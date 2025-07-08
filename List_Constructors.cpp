be06f3774f544f07974ddc3941a898d189a91047
nafis200 <nafisahamed14@gmail.com>
2025-07-08 20:18:41 +0600

    list

--- List_Constructors.cpp
+++ List_Constructors.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 int main(){
     list<int>l(20,3);
+    list<int>l2(l);
+    int arr[] = {1,2,3,4};
+     list<int>l3(arr, arr + 4);
     cout << l.size() << "\n";
     // list dosent print by index.
 
-    for(auto it = l.begin(); it != l.end(); it++){
-        cout << *it << "\n";
+    // for(auto it = l.begin(); it != l.end(); it++){
+    //     cout << *it << "\n";
+    // }
+
+    for(auto val : l3){
+        cout << val << "\n";
     }
 }
